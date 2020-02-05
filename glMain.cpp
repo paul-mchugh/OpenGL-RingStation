fafeca7e13ec23ff965fc4950a2686a65480cdd8
Paul McHugh <paulpjmmchugh@gmail.com>
2020-02-05 14:30:20 -0800

    Added zoom and help message

--- glMain.cpp
+++ glMain.cpp
@@ -14,6 +14,8 @@ GLuint vao[numVAOs];
 void init(GLFWwindow* window);
 void display(GLFWwindow* window, double currentTime);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void scroll_callback(GLFWwindow* window, double xScrOff, double yScrOff);
+void printHelp(void);
 
 //triangle action
 enum class TriAction
@@ -23,8 +25,10 @@ enum class TriAction
 	CIRCULAR
 };
 TriAction state = TriAction::HORIZONTAL;
-bool colorIntr = false;
+bool colorIntr = true;
 double lastTime=0;
+double zoomDelta=0.05;
+double zoom = 0.5;
 double deltaPerSec=0.1;
 double hProgress=1.0;
 double vProgress=0.0;
@@ -60,9 +64,11 @@ void init(GLFWwindow* window)
 	renderingProgram = Util::createShaderProgram("vertShader.glsl", "fragShader.glsl");
 	if(!renderingProgram) std::cout << "Could not Load shader" << std::endl;
 	glfwSetKeyCallback(window, key_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 	glGenVertexArrays(numVAOs, vao);
 	glBindVertexArray(vao[0]);
 	Util::printGLInfo();
+	printHelp();
 }
 
 void display(GLFWwindow* window, double currentTime)
@@ -96,6 +102,7 @@ void display(GLFWwindow* window, double currentTime)
 	hProgress=std::min<double>(1,std::max<double>(0,hProgress));
 	vProgress=std::min<double>(1,std::max<double>(0,vProgress));
 	cProgress=std::min<double>(1,std::max<double>(0,cProgress));
+	zoom=std::min<double>(1,std::max<double>(0,zoom));
 
 	//position the triangle based off the progress, etc.
 	double periodProgress = fmod(currentTime, 4);
@@ -112,9 +119,11 @@ void display(GLFWwindow* window, double currentTime)
 	GLuint xOffHandle = glGetUniformLocation(renderingProgram, "xOff");
 	GLuint yOffHandle = glGetUniformLocation(renderingProgram, "yOff");
 	GLuint intrHandle = glGetUniformLocation(renderingProgram, "intr");
+	GLuint zoomHandle = glGetUniformLocation(renderingProgram, "zoom");
 	glProgramUniform1f(renderingProgram, xOffHandle, x);
 	glProgramUniform1f(renderingProgram, yOffHandle, y);
 	glProgramUniform1i(renderingProgram, intrHandle, colorIntr);
+	glProgramUniform1f(renderingProgram, zoomHandle, zoom);
 
 	glDrawArrays(GL_TRIANGLES, 0,3);
 }
@@ -128,7 +137,13 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
 	}
 
-	if(key == GLFW_KEY_H && action == GLFW_PRESS)
+	if((key == GLFW_KEY_SLASH || key == GLFW_KEY_F1) && action == GLFW_PRESS)
+	{
+		//I know that I am skippiny the shift+/ part of the ? key, but I
+		//want it to be easy to press the help key
+		printHelp();
+	}
+	else if(key == GLFW_KEY_H && action == GLFW_PRESS)
 	{
 		state = TriAction::HORIZONTAL;
 		printf("Pressed key H: Horizontal triangle movement\n");
@@ -148,4 +163,42 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		colorIntr = !colorIntr;
 		printf("Pressed key I: %s color interpolation\n", (colorIntr ? "Enabled":"Disabled"));
 	}
+	else if((key == GLFW_KEY_0 || key == GLFW_KEY_KP_0) && action == GLFW_PRESS)
+	{
+		zoom = 0.5;
+		printf("Pressed key 0: Zoom reset\n");
+	}
+	else if((key == GLFW_KEY_KP_ADD || key == GLFW_KEY_EQUAL) && action != GLFW_RELEASE)
+	{
+		zoom += zoomDelta;
+		printf("Pressed key +: Zoom %s\n", (zoom>1.025?"at max":"increased"));
+	}
+	else if((key == GLFW_KEY_MINUS || key == GLFW_KEY_KP_SUBTRACT) && action != GLFW_RELEASE)
+	{
+		zoom -= zoomDelta;
+		printf("Pressed key -: Zoom %s\n", (zoom<-0.025?"at min":"decreased"));
+	}
+}
+
+void scroll_callback(GLFWwindow* window, double xScrOff, double yScrOff)
+{
+	if(yScrOff>0)		zoom += zoomDelta;
+	else if(yScrOff<0)	zoom -= zoomDelta;
+}
+
+void printHelp(void)
+{
+	printf(
+		"Usage:\n"
+		"Press any of the following keys while the window is selected\n"
+		" ?:  View this help message in console(F1 also works)\n"
+		" H:  Switch triangle to horizontal movement mode(default mode)\n"
+		" V:  Switch triangle to vertical movement mode\n"
+		" C:  Switch triangle to circular movement mode\n"
+		" I:  Toggle Color Interpolation(default on)\n"
+		" 0:  Reset Zoom\n"
+		" +:  Increase Zoom(or scroll)\n"
+		" -:  Decrease Zoom(or scroll)\n"
+		"^Q:  Quit program(^W also works)\n"
+	);
 }
--- vertShader.glsl
+++ vertShader.glsl
@@ -1,22 +1,24 @@
 #version 430
 uniform float xOff;
 uniform float yOff;
+uniform float zoom;
 out vec4 colorIntr;
 void main(void)
 {
+	float dist = 0.25*(0.75*zoom+0.25);
 	switch(gl_VertexID)
 	{
 	case 0:
-		gl_Position = vec4(0.25+xOff,-0.25+yOff,0.0,1.0);
+		gl_Position = vec4(dist+xOff,-dist+yOff,0.0,1.0);
 		colorIntr   = vec4(1.0, 0.0, 0.0, 1.0);
 		break;
 	case 1:
-		gl_Position = vec4(-0.25+xOff,-0.25+yOff,0.0,1.0);
+		gl_Position = vec4(-dist+xOff,-dist+yOff,0.0,1.0);
 		colorIntr   = vec4(0.0, 1.0, 0.0, 1.0);
 		break;
 	default:
 	case 2:
-		gl_Position = vec4(xOff,0.25+yOff,0.0,1.0);
+		gl_Position = vec4(xOff,dist+yOff,0.0,1.0);
 		colorIntr   = vec4(0.0, 0.0, 1.0, 1.0);
 		break;
 	}
